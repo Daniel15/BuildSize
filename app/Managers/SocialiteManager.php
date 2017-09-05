4a1e5fae3e20f4de10343e70d49414152f11da44
Daniel Lo Nigro <daniel@dan.cx>
2017-09-04 18:17:30 -0700

    Fix GitHub login redirect URL

--- app/Managers/SocialiteManager.php
+++ app/Managers/SocialiteManager.php
@@ -8,7 +8,7 @@ use Laravel\Socialite\SocialiteManager as BaseSocialiteManager;
 class SocialiteManager extends BaseSocialiteManager {
   protected function createGithubDriver() {
     $config = $this->app['config']['services.github'];
-
+    $config['redirect'] = action('GithubAuthController@completeLogin');
     return $this->buildProvider(GithubProvider::class, $config);
   }
 }
