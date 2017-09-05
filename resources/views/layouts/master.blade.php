8eeb13eae2b6cb784cf6738ae6ab6a9243a4069e
Daniel Lo Nigro <daniel@dan.cx>
2017-09-04 21:10:06 -0700

    Add Google Analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,15 @@
   </title>
   <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
 
-  <!-- TODO: analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-25623237-10', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
 <body>
 @include('layouts._partial.navbar')
