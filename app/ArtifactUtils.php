0136532cfa3a0f851331574b695be1e81a7dcff0
Bertrand Marron <bertrand.marron@gmail.com>
2017-12-11 15:18:08 +0100

    Strip hashes from file names

--- app/ArtifactUtils.php
+++ app/ArtifactUtils.php
@@ -3,6 +3,7 @@
 namespace App;
 
 abstract class ArtifactUtils {
+  const HASH_PLACEHOLDER = '[hash]';
   const VERSION_PLACEHOLDER = '[version]';
 
   /**
@@ -12,6 +13,14 @@ abstract class ArtifactUtils {
    * @return string
    */
   public static function generalizeName(string $name): string {
+    // Strip hashes
+    // Match hex hashes betwen 20 (webpack truncated default) and 64 (sha256) characters.
+    $name = preg_replace(
+      '/([a-f0-9]{20,64})/i',
+      static::HASH_PLACEHOLDER,
+      $name
+    );
+
     // Strip version numbers
     $name = preg_replace(
       '/([0-9]+(\.[0-9_-]+)+)/',
