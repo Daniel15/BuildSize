bf65d8b78426fa3fc597a0593cc9e2dc5cc7bc5b
Fabian Wiles <fabianwiles@live.com>
2018-03-04 21:30:21 +1300

    display status of largest diff instead of total

--- app/Listeners/GitHubStatusListener.php
+++ app/Listeners/GitHubStatusListener.php
@@ -20,27 +20,42 @@ class GitHubStatusListener {
    * @return void
    */
   public function handle(BuildCompletedEvent $event) {
-    $description = 'Build size: ' . Format::fileSize($event->total_size);
     $state = 'success';
-
     if ($event->has_base_build) {
-      // Can compare to base
-      $diff = $event->base_total_size - $event->total_size;
-      if (abs($diff) === 0) {
-       $description .= ' (no change)';
+
+      $largestChange = null;
+      $largestDiff = 0;
+
+      foreach ($event->build_artifacts as $artifact) {
+        if (!$event->base_build_artifacts->has($artifact->project_artifact_id)) {
+          // No base version, so nothing to compare to
+          continue;
+        }
+        $base_artifact = $event->base_build_artifacts->get($artifact->project_artifact_id);
+        $diff = $base_artifact->size - $artifact->size;
+        if(abs($diff) > $largestDiff) {
+          $largestChange = $artifact;
+          $largestDiff = $diff;
+        }
+      }
+
+      if($largestDiff === 0) {
+        $description = 'No change';
       } else if (abs($diff) < config('buildsize.github.trivial_size')) {
-        $description .= ' (no significant change)';
+        $description = 'No significant change';
       } else {
         $diff_percent = round($diff / $event->base_total_size * 100.0, 2);
         if ($diff > 0) {
-          $description .= ' (decreased by ' . Format::fileSize($diff) . ', ' . $diff_percent . '%)';
+          $description = 'Significant change of ' . $largestChange->filename . ' down by ' . Format::fileSize($diff) . ' (' . $diff_percent . '%)';
         } else {
-          $description .= ' (increased by ' . Format::fileSize(-$diff) . ', ' . -$diff_percent . '%)';
+          $description = 'Significant change of ' . $largestChange->filename . ' up by ' . Format::fileSize(-$diff) . ' (' . -$diff_percent . '%)';
           if ($diff < static::WARN_THRESHOLD) {
             //$state = 'failure';
           }
         }
       }
+    } else {
+      $description = 'No prior size to compare - ' . Format::fileSize($event->total_size);
     }
 
     $github = GithubUtils::createClientForInstall($event->install);
@@ -49,7 +64,7 @@ class GitHubStatusListener {
       $event->project->repo_name,
       $event->build->commit,
       [
-        'context' => config('buildsize.github.status_context_prefix') . '/total',
+        'context' => config('buildsize.github.status_context_prefix'),
         'description' => $description,
         'state' => $state,
         'target_url' => 'https://buildsize.org/', // TODO
