b1c20efe1825855108677459f764cd41b83fbe9c
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-06 01:15:07 +0100

    Disable all library hooks besides curl for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require(__DIR__ . '/../vendor/autoload.php');
+\VCR\VCR::configure()->enableLibraryHooks(array('curl'));
 \VCR\VCR::turnOn();
 \VCR\VCR::configure()
   ->setCassettePath(__DIR__ . '/fixtures/http/')
